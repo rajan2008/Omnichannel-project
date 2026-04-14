04349c326024177e2e9afd444a4fa796411562b7
Sahibealam2003 <alamsahib9719@gmail.com>
2026-04-14 19:24:19 +0530

    model and connectdb done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connect from "./src/config/connectdb.js";
 
-// ENV Setup
 dotenv.config();
-
-// Express Init
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -20,6 +17,16 @@ app.get('/', (req, res) => {
 // Port Setup
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const startServr = async () => {
+  try {
+    await connect();  
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port " + process.env.PORT);
+    });
+  } catch (error) {
+    console.log("Server failed to start");
+    process.exit(1);   
+  }
+};
+
+startServr()
--- backend/src/config/connectdb.js
+++ backend/src/config/connectdb.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/omnichannel");
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.error('MongoDB connection failed', error);
-        process.exit(1);
-    }
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database is connect");
+  } catch (error) {
+    console.log("Database is not connect");
+    throw error;   
+  }
 };
 
-export default connectDB;
+export default connect;
