cba0632e87bf2c5f182fe850e652f19a1f8e8063
Rajan Prajapati <159687563+rajan2008@users.noreply.github.com>
2026-05-06 13:21:16 +0530

    Fix: Made SMTP configuration dynamic via env variables (HOST, PORT, SECURE)

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -21,9 +21,9 @@ const sendEmail = async (options) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, // true for 465
+      host: process.env.EMAIL_HOST || "smtp.gmail.com",
+      port: parseInt(process.env.EMAIL_PORT) || 465,
+      secure: process.env.EMAIL_SECURE ? process.env.EMAIL_SECURE === "true" : true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
